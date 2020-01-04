5e969c564bc2299799bca4f2fe174109aa4728ab
John Moshakis <john.moshakis@firmex.com>
2020-01-04 04:33:06 -0500

    2 async templates

---
+++ Oxygene/Echoes/Projects/Core/AsyncConsoleApplication/Program.pas
@@ -0,0 +1,17 @@
+﻿namespace $safeprojectname$;
+
+$if$ ($targetframeworkversion$ >= 3.5)
+uses
+  System.Linq,
+  System.Threading.Tasks;
+$endif$
+type
+  Program = class
+  public
+    class method Main(args: array of String): Task<Int32>;
+    begin
+      await Task.Delay(TimeSpan.FromSeconds(2));
+    end;
+  end;
+
+end.
---
+++ Oxygene/Echoes/Projects/Windows/AsyncConsoleApplication/Program.pas
@@ -0,0 +1,17 @@
+﻿namespace $safeprojectname$;
+
+uses
+  System.Threading.Tasks;
+
+type
+  Program = class
+  public
+
+    class method Main(args: array of String): Task<Int32>;
+    begin
+      await Task.Delay(TimeSpan.FromSeconds(2));
+    end;
+
+  end;
+
+end.
