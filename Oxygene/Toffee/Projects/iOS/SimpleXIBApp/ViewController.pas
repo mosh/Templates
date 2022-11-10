db10095d67eda128472b2e72b10d23d8e2f426c8
John Moshakis <john@moshakis.com>
2022-11-10 17:16:05 -0500

    SimpleXIB template

---
+++ Oxygene/Toffee/Projects/iOS/SimpleXIBApp/ViewController.pas
@@ -0,0 +1,40 @@
+﻿namespace SimpleApp;
+
+uses
+  UIKit;
+
+type
+  [IBObject]
+  ViewController = public class(UIViewController)
+  private
+  public
+    method init: InstanceType; override;
+    begin
+      self := inherited initWithNibName('ViewController') bundle(nil);
+      if assigned(self) then begin
+
+        // Custom initialization
+
+      end;
+      result := self;
+    end;
+
+
+    method viewDidLoad; override;
+    begin
+      inherited viewDidLoad;
+
+      // Do any additional setup after loading the view.
+    end;
+
+    method didReceiveMemoryWarning; override;
+    begin
+      inherited didReceiveMemoryWarning;
+
+      // Dispose of any resources that can be recreated.
+    end;
+
+
+  end;
+
+end.
