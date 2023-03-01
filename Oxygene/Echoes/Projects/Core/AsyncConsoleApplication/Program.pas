68a5f2ce361c4cadfcbc33322da2f60649c878af
John Moshakis <john@moshakis.com>
2023-02-28 22:16:43 -0500

    simplify

--- Oxygene/Echoes/Projects/Core/AsyncConsoleApplication/Program.pas
+++ Oxygene/Echoes/Projects/Core/AsyncConsoleApplication/Program.pas
@@ -5,13 +5,8 @@ uses
   System.Linq,
   System.Threading.Tasks;
 $endif$
-type
-  Program = class
-  public
-    class method Main(args: array of String): Task;
-    begin
-      await Task.Delay(TimeSpan.FromSeconds(2));
-    end;
-  end;
+
+begin
+  await Task.Delay(TimeSpan.FromSeconds(2));
 
 end.
--- Oxygene/Echoes/Projects/Windows/AsyncConsoleApplication/Program.pas
+++ Oxygene/Echoes/Projects/Windows/AsyncConsoleApplication/Program.pas
@@ -3,15 +3,6 @@
 uses
   System.Threading.Tasks;
 
-type
-  Program = class
-  public
-
-    class method Main(args: array of String): Task;
-    begin
-      await Task.Delay(TimeSpan.FromSeconds(2));
-    end;
-
-  end;
-
+begin
+  await Task.Delay(TimeSpan.FromSeconds(2));
 end.
